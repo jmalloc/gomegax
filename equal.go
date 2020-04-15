de25404630309af5c5a486932e5593e73d3d0a04
James Harris <mailjamesharris@gmail.com>
2020-04-15 15:24:16 +1000

    Initial commit.

---
+++ equal.go
@@ -0,0 +1,48 @@
+package gomegax
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"github.com/onsi/gomega/format"
+	"github.com/onsi/gomega/types"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+// EqualX is an alternative to gomega.Equal() that uses Google's go-cmp library
+// for comparison.
+//
+// By default it uses the protocmp transform, making it useful for comparing
+// protobuf messages.
+func EqualX(
+	expected interface{},
+	options ...cmp.Option,
+) types.GomegaMatcher {
+	if len(options) == 0 {
+		options = append(options, protocmp.Transform())
+	}
+
+	return &equalMatcher{
+		expected: expected,
+		options:  options,
+	}
+}
+
+type equalMatcher struct {
+	expected interface{}
+	options  cmp.Options
+}
+
+func (m *equalMatcher) Match(actual interface{}) (success bool, err error) {
+	return cmp.Equal(actual, m.expected, m.options), nil
+}
+
+func (m *equalMatcher) FailureMessage(actual interface{}) (message string) {
+	diff := cmp.Diff(actual, m.expected, m.options)
+	return format.Message(actual, "to equal", m.expected) +
+		"\n\nDiff:\n" + format.IndentString(diff, 1)
+}
+
+func (m *equalMatcher) NegatedFailureMessage(actual interface{}) (message string) {
+	diff := cmp.Diff(actual, m.expected, m.options)
+	return format.Message(actual, "not to equal", m.expected) +
+		"\n\nDiff:\n" + format.IndentString(diff, 1)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/jmalloc/gomegax
+
+go 1.14
+
+require (
+	github.com/google/go-cmp v0.4.0
+	github.com/onsi/gomega v1.9.0
+	google.golang.org/protobuf v1.21.0
+)
